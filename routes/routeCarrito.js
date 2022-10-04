07be1cb96bbb82709859662097f28635bb1bc319
Rodrigo <rodridega@gmail.com>
2022-10-03 23:04:46 -0300

    test routers

--- routes/routeCarrito.js
+++ routes/routeCarrito.js
@@ -7,37 +7,37 @@ const routerCarrito = Router()
 let administrador = true
 
 routerCarrito.post('/', async(req, res) => { //Recibe y agrega un carrito, y devuelve su id asignado
-    let allCarrito = await carritosApi.getAll()
-    let newId
+    let todosLosCarritos = await carritosApi.getAll()
+    let nuevoId
     let fecha = new Date()
 
-    if(allCarrito.length === 0){
-        newId=1
+    if(todosLosCarritos.length === 0){
+        nuevoId=1
     }else{
-        newId = parseInt(allCarrito[allCarrito.length-1].id) + 1
+        nuevoId = parseInt(todosLosCarritos[todosLosCarritos.length-1].id) + 1
     }
 
-    allCarrito.push({id: newId, 'timestamp(carrito)': fecha, productos: []})
-    await carritosApi.saveAll(allCarrito)
-    res.send({id: newId})       
+    todosLosCarritos.push({id: nuevoId, 'timestamp(carrito)': fecha, productos: []})
+    await carritosApi.saveAll(todosLosCarritos)
+    res.send({id: nuevoId})       
 })
 
 routerCarrito.delete('/:id', async(req, res) => { //Elimina un carrito según su id   
-    let allCarrito = await carritosApi.getAll()  
-    const iD = allCarrito.find(cart => parseInt(cart.id) === parseInt(req.params.id))
+    let todosLosCarritos = await carritosApi.getAll()  
+    const iD = todosLosCarritos.find(cart => parseInt(cart.id) === parseInt(req.params.id))
 
     if (!iD) {
-        res.status(400).json({ error : "Carrito no encontrado" });
+        res.status(400).json({ error : "No existe el carrito que estas buscando" });
     } else {
         await carritosApi.deleteById(parseInt(req.params.id))
-        allCarrito = await carritosApi.getAll()
-        res.send(allCarrito)
+        todosLosCarritos = await carritosApi.getAll()
+        res.send(todosLosCarritos)
     }   
 })
 
 routerCarrito.get('/:id/productos', async(req, res) => { //Devuelve los productos de un carrito segun su id
-    let allCarrito = await carritosApi.getAll()
-    const iD = allCarrito.find(cart => parseInt(cart.id) === parseInt(req.params.id))
+    let todosLosCarritos = await carritosApi.getAll()
+    const iD = todosLosCarritos.find(cart => parseInt(cart.id) === parseInt(req.params.id))
 
     if (iD) {
         res.send(iD.productos)
@@ -47,19 +47,20 @@ routerCarrito.get('/:id/productos', async(req, res) => { //Devuelve los producto
 })
 
 routerCarrito.post('/:id/productos', async(req, res) => { //Incorporar productos al carrito por su id de producto
-    let allCarrito = await carritosApi.getAll()
+    let todosLosCarritos = await carritosApi.getAll()
+   
+    const iDCart = todosLosCarritos.find(cart => parseInt(cart.id) === parseInt(req.params.id))
     
-    const iDCart = allCarrito.find(cart => parseInt(cart.id) === parseInt(req.params.id))
-    const indexCart = allCarrito.map(cart => parseInt(cart.id)).indexOf(parseInt(req.params.id))
+    const indexCart = todosLosCarritos.map(cart => parseInt(cart.id)).indexOf(parseInt(req.params.id))
 
     if (iDCart) {
         const { idProducto } = req.body
         let allProductos = await productosApi.getAll()
         const iD = allProductos.find(producto => parseInt(producto.id) === idProducto)
-
+        console.log(allProductos);
         if (iD) {
-            allCarrito[indexCart].productos.push(iD)
-            await carritosApi.saveAll(allCarrito)
+            todosLosCarritos[indexCart].productos.push(iD)
+            await carritosApi.saveAll(todosLosCarritos)
             res.send(`Producto con ID: ${idProducto} agregado exitosamente al carrito con ID: ${req.params.id}`)    
         }else{
             res.status(400).json({ "error": "Ingrese el ID del producto" })
@@ -71,18 +72,18 @@ routerCarrito.post('/:id/productos', async(req, res) => { //Incorporar productos
 })
 
 routerCarrito.delete('/:id/productos/:id_prod', async(req, res) => { //Elimina un producto del carrito por su id de carrito y de producto
-    let allCarrito = await carritosApi.getAll()
-    const iDCart = allCarrito.find(cart => parseInt(cart.id) === parseInt(req.params.id))
-    const indexCart = allCarrito.map(cart => parseInt(cart.id)).indexOf(parseInt(req.params.id))
+    let todosLosCarritos = await carritosApi.getAll()
+    const iDCart = todosLosCarritos.find(cart => parseInt(cart.id) === parseInt(req.params.id))
+    const indexCart = todosLosCarritos.map(cart => parseInt(cart.id)).indexOf(parseInt(req.params.id))
 
     if (iDCart) {
-        const arrayProd = allCarrito[indexCart].productos
+        const arrayProd = todosLosCarritos[indexCart].productos
         const iDProd = arrayProd.find(producto => parseInt(producto.id) === parseInt(req.params.id_prod))
 
         if (iDProd) {
             const filterId = arrayProd.filter((item) => parseInt(item.id) !== parseInt(req.params.id_prod))
-            allCarrito[indexCart].productos = filterId
-            await carritosApi.saveAll(allCarrito)
+            todosLosCarritos[indexCart].productos = filterId
+            await carritosApi.saveAll(todosLosCarritos)
             res.send(`Producto Eliminado con exito`) 
         } else {
             res.status(400).json({ error : "Producto no encontrado" });
