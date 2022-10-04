c9d88d4bf6d81f55768239f826c2a1a3334f4de8
rodridega <56453875+rodridega@users.noreply.github.com>
2022-10-04 13:18:27 -0300

    Arregla key para alojar en mongoSchema

--- daos/productos/productosDaoMongo.js
+++ daos/productos/productosDaoMongo.js
@@ -6,11 +6,11 @@ class ProductosDaoMongo extends ContenedorMongo {
         super('productos', {
             id: {type: Number, require: true, max: 255},
             'timestamp(producto)': {type: String, require: true, max: 100},
-            nombre: {type: String, require: true, max: 100},
-            descripcion: {type: String, require: true, max: 100},
-            codigo: {type: String, require: true, max: 100},
-            foto: {type: String, require: true, max: 100},
-            precio: {type: String, require: true, max: 100},
+            name: {type: String, require: true, max: 100},
+            description: {type: String, require: true, max: 100},
+            code: {type: String, require: true, max: 100},
+            urlImage: {type: String, require: true, max: 100},
+            price: {type: String, require: true, max: 100},
             stock: {type: String, require: true, max: 100}            
         })
     }
--- utils/databaseSwitcher.js
+++ utils/databaseSwitcher.js
@@ -1,6 +1,6 @@
 let databases = ['txt','firebase','mongo'] 
 
-//--- cambiar el indix del array para elegir que DB usar
-let switcher = databases[1]
+//--- cambiar el indice del array para elegir que DB usar
+let switcher = databases[2]
 
 export { switcher }
